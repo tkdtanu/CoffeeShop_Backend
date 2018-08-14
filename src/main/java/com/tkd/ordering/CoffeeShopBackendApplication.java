2e4447bb2eacff995b6f90aa71276819aea75aa1
Tarun Kumar Das <tkdtanu@gmail.com>
2018-08-14 23:08:40 +0530

    initial project commit

---
+++ src/main/java/com/tkd/ordering/CoffeeShopBackendApplication.java
@@ -0,0 +1,12 @@
+package com.tkd.ordering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeShopBackendApplication.class, args);
+	}
+}
